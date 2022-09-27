9d60436f55b063ef8287f6b46cc137cbbf85e83f
amitmaindola <amitkm.knp2019@gmail.com>
2022-09-27 21:10:56 +0530

    Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,117 @@
 var sections = [];
-var colors = ["#81ecec", "#fab1a0", "#fdcb6e", "#74b9ff", "#55efc4"]
+var colors = ["#81ecec", "#fab1a0", "#fdcb6e", "#74b9ff", "#55efc4"];
+var links = document.querySelectorAll(".navLink");
 sections.push(document.getElementById("about"));
 sections.push(document.getElementById("education"));
 sections.push(document.getElementById("skills"));
 sections.push(document.getElementById("projects"));
 sections.push(document.getElementById("positions"));
 
+var main = document.getElementById("main");
+
 function goToSection(x) {
-    for (let i = 0; i < sections.length; i++) {
-        const element = sections[i];
-        if(i==x) element.style.display = "flex";
-        else element.style.display="none";
-    }
+    main.style.transform="translateX(300px) scale(0.8)";
+    // main.style.transform="rotate(-60deg)";
+    setTimeout(() => {
+        for (let i = 0; i < sections.length; i++) {
+            const element = sections[i];
+            if(i==x){
+                element.style.display = "flex";
+                // links[i].style.borderBottom = "2px dashed #ccf";
+                links[i].style.color="#ccf";
+            }
+            else{
+                element.style.display="none";
+                // links[i].style.borderBottom = "none";
+                links[i].style.color = "#fff";
+            }
+            main.style.background = colors[x];
+        }
+        main.style.transform="translateX(0px) scale(1)";
+        // main.style.transform="rotate(0deg)";
+    }, 1000);
 }
+
+
+// For typer effect
+var text="";
+const fullText1 = "< Competetive Programmer_/>";
+const fullText2 = "< Full Stack Developer _/>";
+const fullText3 = "< Machine Learning Enthusiast_/>";
+const texts=[""];
+for (let i = 1; i <= fullText1.length; i++) {
+    text=fullText1.slice(0,i);
+    texts.push(text);
+}
+for (let i = 0; i < 10; i++) {
+    texts.push(fullText1);
+}
+for (let i = fullText1.length; i >= 0; i=i-2) {
+    text=fullText1.slice(0,i);
+    texts.push(text);
+}
+for (let i = 1; i <= fullText2.length; i++) {
+    text=fullText2.slice(0,i);
+    texts.push(text);
+}
+for (let i = 0; i < 10; i++) {
+    texts.push(fullText2);
+}
+for (let i = fullText2.length; i >= 0; i=i-2) {
+    text=fullText2.slice(0,i);
+    texts.push(text);
+}
+for (let i = 1; i <= fullText3.length; i++) {
+    text=fullText3.slice(0,i);
+    texts.push(text);
+}
+for (let i = 0; i < 10; i++) {
+    texts.push(fullText3);
+}
+for (let i = fullText3.length; i >= 0; i=i-2) {
+    text=fullText3.slice(0,i);
+    texts.push(text);
+}
+
+
+
+var i=0,j=0, j2=0;
+const arrayLen = texts.length;
+function Screen() {
+   setInterval(function () {
+       if (i==arrayLen) {
+           i=0;
+       }
+       text = texts[i++];
+        document.getElementById("text").innerText=text;
+   },100)
+   setInterval(function(){
+       if(j==0){
+           document.getElementById("span").style.opacity=0;
+       }
+       if(j==1){
+           document.getElementById("span").style.opacity=0.25;
+       }
+       if(j==2){
+           document.getElementById("span").style.opacity=0.5;
+       }
+       if(j==3){
+           document.getElementById("span").style.opacity=1;
+       }
+       if(j==4){
+           document.getElementById("span").style.opacity=0.5;
+       }
+       if(j==5){
+           document.getElementById("span").style.opacity=0.25;
+       }
+       if (j==6) {
+          document.getElementById("span").style.opacity=0;
+       }
+       j++;
+       if (j==7) {
+        j=0;
+       }
+   },100);
+}
+
+Screen();
